4f7033ee0a652308d8dfc03139f39e877b7aa784
Bodo <bodo.burger@gmail.com>
2018-06-11 15:35:04 +0200

    Basic plot function

---
+++ R/plotAME.R
@@ -0,0 +1,36 @@
+#' Plot AME intervalls
+#'
+#' @param object Model object
+#' @param data data.frame
+#' @param ame AME object
+#' @param target character(1) Name of target variable.
+#'
+#' @section TODOS:
+#' \itemize{
+#' \item Wrapped.Model / task support
+#' \item classification support
+#' \item Facet of different models (i.e. draw multiple AME lines for one feature)
+#' }
+#'
+#' @export
+plotAME = function(object, data, ame, target) {
+  features = names(ame)
+  data = data[, c(features, target)]
+  target.mean = mean(data[, target])
+  features.mean = colMeans(data[, features, drop = FALSE])
+  intercepts = mean(data[, target]) - ame * features.mean
+
+  data.plot = reshape2::melt(data, id.vars = target, variable.name = "feature", value.name = "x")
+  data.plot[["y.ame"]] = numeric(nrow(data.plot))
+  for (feature in features) {
+    data.plot[data.plot$feature==feature, "y.ame"] =
+      intercepts[feature] + data.plot[data.plot$feature == feature, "x"] * ame[feature]
+  }
+  #ggplot() +
+  #  geom_point(data = data, mapping = aes_(x = as.name(features), y = as.name(target)), alpha = .3) +
+  #  geom_abline(mapping = aes(slope = ame, intercept = intercept, color = color.group), show.legend = show.legend)
+  ggplot2::ggplot(data = data.plot, mapping = aes(x = x)) +
+    ggplot2::geom_point(mapping = aes(y = y), alpha = .3) +
+    ggplot2::geom_line(mapping = aes(y = y.ame), col = "red") +
+    ggplot2::facet_wrap(~ feature, scales = "free_x")
+}
