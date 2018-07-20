f7f257fa9663dc5c794e5f01825658d541cae4cc
BodoBurger <bodo.burger@gmail.com>
2018-07-20 11:58:45 +0200

    Update computeALE

--- R/computeALE.R
+++ R/computeALE.R
@@ -3,6 +3,10 @@
 #' @section TODO:
 #' \itemize{
 #'   \item implement mlr models
+#'   \item factor features
+#'   \item minbucket
+#'   \item pass specific interval boundaries (z)
+#'   \item second-order effects
 #' }
 #'
 #' @param data a data.frame
@@ -14,11 +18,18 @@
 #' @export
 #'
 #' @examples
-computeALE = function(model, data, feature, K, predict.fun = predict) {
-  z = as.numeric(quantile(data[, feature], seq(0, 1, length.out = K+1), type = 1))
-  z = unique(z) # if K > nrow(data) or data[, feature] has lots of non-unique values
+computeALE = function(model, data, feature, K, predict.fun = predict, minbucket = 5) {
+  x = data[, feature]
+  if (K >= length(x)-1) z = sort(x)
+  else {
+    #z = as.numeric(quantile(x, seq(0, 1, length.out = K+1), type = 1))
+    z = c(min(x), as.numeric(quantile(x, seq(1/K, 1, length.out = K),
+      type = 1))) # c(min(), quantile()) necessary for K = n
+    z = unique(z) # if K > nrow(data) or x has lots of non-unique values
+  }
   K = length(z) - 1
-  interval.indices = as.numeric(cut(data[, feature], breaks = z, include.lowest = TRUE))
+  # if K >= (n-1) the first two obs are assigned to the first interval
+  interval.indices = as.numeric(cut(x, breaks = z, include.lowest = TRUE))
   data.l = data
   data.u = data
   data.l[, feature] = z[interval.indices]
@@ -27,10 +38,11 @@ computeALE = function(model, data, feature, K, predict.fun = predict) {
   y.hat.u = predict.fun(object = model, newdata = data.u)
   delta = y.hat.u - y.hat.l
   delta = as.numeric(tapply(delta, interval.indices, mean)) # provide alternative aggregation function
-  fJ = c(0, cumsum(delta))
-  b1 = as.numeric(table(a1))
-  fJ = fJ - sum((fJ[1:K] + fJ[2:(K + 1)])/2 * b1)/sum(b1)
-  x = z
+  #f = cumsum(c(mean(y.hat.l[interval.indices == 1]), delta))
+  f = c(0, cumsum(delta))
+  w = as.numeric(table(interval.indices))
+  f = f - sum((f[1:K] + f[2:(K + 1)])/2 * w) / sum(w)
+  return(list(x = z, f = f, K = K, i = interval.indices, ale = delta/diff(z)))
 }
 
 #plot(x, fJ, type = "l", xlab = paste("x_", J, " (",
--- test_computeALE.R
+++ test_computeALE.R
@@ -1,5 +1,5 @@
-
-n = 40
+devtools::load_all()
+n = 200
 x = runif(n, min = 0, max = 1)
 x1 = x + rnorm(n, 0, 0.05)
 x2 = x + rnorm(n, 0, 0.05)
@@ -11,7 +11,7 @@ y1 = function(x) x
 y2 = function(x) 10*(x-.25)^2
 y3 = function(x) -2 * x
 y4 = function(x) 40 * ((x-.2)^3 - (x-.2)^2) + 4
-y5 = function(x) -4 * cos(4*pi*x) * x
+y5 = function(x) -4 * cos(4*pi*x) * x + 4
 
 x.grid = seq(0, 1, .01)
 par(mfrow = c(1,1))
@@ -30,3 +30,21 @@ tsk = makeRegrTask(data = dt, target = "y")
 nnet.lrn = makeLearner("regr.nnet", skip = FALSE, size = 20, decay = 0.0001, maxit = 1000,
   trace = FALSE)
 nnet.mod = train(nnet.lrn, tsk)
+
+ALEPlot::ALEPlot(dt[-1], nnet.mod$learner.model, pred.fun = function(X.model, newdata)
+  predict(X.model, newdata), J = 5, K = 50)
+ALE = computeALE(nnet.mod$learner.model, dt, "x5", K = 40)
+computeALE(nnet.mod$learner.model, dt, "x5", K = 9)
+lines(ALE$x, ALE$f, col = "blue", lty = 2, lwd = 2)
+
+nnet.pd = generatePartialDependenceData(nnet.mod, tsk, "x5", n = 40, uniform = FALSE)
+
+x.grid = seq(0, 1, .01)
+f.hat = predict(nnet.mod$learner.model, dt)
+c = f.hat[which.min(dt$x5)]
+plot(dt$x5, f.hat, col = "red")
+lines(ALE$x, ALE$f + mean(f.hat), col = "blue", lty = 2, lwd = 2)
+lines(nnet.pd$data$x5, nnet.pd$data$y, col = "green")
+lines(x.grid, y5(x.grid) - y5(0) + mean(f.hat), type = "l", col = "black")
+abline(v = ALE$x, lwd = .5)
+
