0a6555d7ecef642b4ae54a5132370fcf3cc6b76a
scholbeck <scholbeck@gmx.de>
2018-11-25 15:18:43 +0100

    -removed derivatives for logical and character features - differentiate between derivative computation for numeric and factor features

--- R/derivative.R
+++ R/derivative.R
@@ -40,22 +40,22 @@ derivative.factor = function(x, feature, data, model,
   return(out)
 }
 
-derivative.logical = function(x, feature, data, model,
-  predict.fun = function(object, newdata) predict(object, newdata = newdata), ...) {
-  lvl = unique(x)
-  out = setNames(lapply(lvl, function(lev) {
-    predictModifiedData(x = lev, feature = feature, data = data,
-      model = model, predict.fun = predict.fun)
-  }), lvl)
-  return(out)
-}
-
-derivative.character = function(x, feature, data, model,
-  predict.fun = function(object, newdata) predict(object, newdata = newdata), ...) {
-  x = as.factor(x) #droplevels(factor(x, levels = levels(data[[feature]])))
-  data[[feature]] = as.factor(data[[feature]])
-  derivative(x, feature, data, model, predict.fun, ...)
-}
+# derivative.logical = function(x, feature, data, model,
+#   predict.fun = function(object, newdata) predict(object, newdata = newdata), ...) {
+#   lvl = unique(x)
+#   out = setNames(lapply(lvl, function(lev) {
+#     predictModifiedData(x = lev, feature = feature, data = data,
+#       model = model, predict.fun = predict.fun)
+#   }), lvl)
+#   return(out)
+# }
+#
+# derivative.character = function(x, feature, data, model,
+#   predict.fun = function(object, newdata) predict(object, newdata = newdata), ...) {
+#   x = as.factor(x) #droplevels(factor(x, levels = levels(data[[feature]])))
+#   data[[feature]] = as.factor(data[[feature]])
+#   derivative(x, feature, data, model, predict.fun, ...)
+# }
 
 # Modify feature in data set and predict using this modified data
 predictModifiedData = function(x, feature, data, model, predict.fun) {
@@ -63,15 +63,16 @@ predictModifiedData = function(x, feature, data, model, predict.fun) {
     prediction.modifdata = predictModifiedDataFactor(
       x, feature, data, model, predict.fun)
   } else {
-    prediction.modifdata = predictModifiedDataGeneral(
+    prediction.modifdata = predictModifiedDataNumeric(
       x, feature, data, model, predict.fun)
   }
   return(prediction.modifdata)
 }
 
-predictModifiedDataGeneral = function(x, feature, data, model, predict.fun) {
-  # modify data and return prediction in case of a numeric feature#
-  # newdata = replace(data, list = which(colnames(data) == feature), values = x)
+predictModifiedDataNumeric = function(x, feature, data, model, predict.fun) {
+  # modify data and return prediction in case of a numeric feature
+  # old command: newdata = replace(data, list = which(colnames(data) == feature),
+  # values = x)
   newdata = data
   newdata[[feature]] = x
   class(newdata[[feature]]) = class(data[[feature]])
@@ -81,7 +82,6 @@ predictModifiedDataGeneral = function(x, feature, data, model, predict.fun) {
 
 predictModifiedDataFactor = function(x, feature, data, model, predict.fun) {
   # modify data and return prediction in case of a factor feature
-
   newdata = data
   feature.levels = levels(data[[feature]])
   newdata[[feature]] = factor(x = x, levels = feature.levels)
